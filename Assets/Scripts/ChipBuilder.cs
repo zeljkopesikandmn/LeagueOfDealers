a164cd3f97f3bc879f621d79af13d0e8141d2c79
Zeljko Pesikan <52235078+zeljkopesikandmn@users.noreply.github.com>
2019-07-03 01:10:15 +0200

    3.7.2019 final
    pocetak scripte za postavljanje cipova na broj

---
+++ Assets/Scripts/ChipBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChipBuilder : MonoBehaviour
+{
+
+	public WhiteChipPool m_whiteChipPool;
+
+	private List<ChipMono> m_activeChips = new List<ChipMono>();
+
+	void Start()
+    {
+        
+    }
+
+
+	private void BuildChips(int inputChips)
+	{
+
+		
+		int numOfChips = inputChips;
+		int chip5s = 0;
+		int singleChips = 0;
+		var chip = m_whiteChipPool.Get();
+		if (chip == null)
+		{
+			Debug.LogWarning("Chip pool is empty!");
+			return;
+		}
+
+		while (numOfChips > 0)
+		{
+			if (numOfChips >= 5)
+			{
+				numOfChips -= 5;
+				chip5s++;
+
+			} 
+
+			if (numOfChips < 5)
+			{
+				numOfChips--;
+				singleChips++;
+			}
+
+
+		}
+	}
+}
